5c8cca70ea4c0ee79ad7f6d7cc89084eada2b5d4
Matheus Santos <vorj.dux@gmail.com>
2020-06-23 12:32:37 +0100

    Fixing the version to the release

--- dist/leaflet.markercluster-src.js
+++ dist/leaflet.markercluster-src.js
@@ -1,5 +1,5 @@
 /*
- * Leaflet.markercluster 1.4.3+v1.4.3.1af3f49,
+ * Leaflet.markercluster 1.4.4+release.fe75882,
  * Provides Beautiful Animated Marker Clustering functionality for Leaflet, a JS library for interactive maps.
  * https://github.com/Leaflet/Leaflet.markercluster
  * (c) 2012-2017, Dave Leaver, smartrak
